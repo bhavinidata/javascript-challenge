7c9439184469268cbc9a938703caeababf42e0b2
bhavini <bhavini266@gmail.com>
2019-10-28 09:34:59 -0500

    used function to populate whole dataset and filtered data into the table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,37 +1,34 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
 let tbody = d3.select("tbody")
-tableData.forEach(sightingData=>{
-    // console.log(sightingData);
-    const trow = tbody.append("tr");
-    columns= ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"];
-    columns.forEach(key=>{
-        // console.log(key, sightingData[key]);
-        trow.append("td").text(sightingData[key]);
+
+// Populate the data into the table
+function poplulateTable(data){
+    // d3.event.preventDefault();
+    data.forEach(sightingData=>{
+        const trow = tbody.append("tr");
+        columns= ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"];
+        columns.forEach(key=>{
+            trow.append("td").text(sightingData[key]);
+        })
     })
-})
+}
+
+// Populate whole dataset into the table while loading the page
+poplulateTable(tableData);
 
 const submit = d3.select("#filter-btn");
 
+// select date and filter data
 submit.on("click", function(){
     d3.event.preventDefault();
     tbody.selectAll('tr').remove()
     const inputDate = d3.select("#datetime").property("value");
     console.log(inputDate);
     const filteredData = tableData.filter(sightingData=>sightingData.datetime === inputDate);
-    console.log(filteredData);
-    filteredData.forEach(sightingData=>{
-        // console.log(sightingData);
-        const trow = tbody.append("tr");
-        columns= ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"];
-        columns.forEach(key=>{
-            // console.log(key, sightingData[key]);
-            trow.append("td").text(sightingData[key]);
-        })
-    })
-
+    // Populate filtered data based on date into the table
+    poplulateTable(filteredData);
 })
 
 
